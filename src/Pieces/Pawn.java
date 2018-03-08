4e92c5bfe34c724ec695b4bd28d4836ded15d93f
Justinian <Justinian@Nano-PC>
2018-03-07 19:07:34 -0500

    Updated a lot of logic for isValidMove in Pawn and ChessPiece

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -6,20 +6,64 @@ import chess.Player;
 
 public class Pawn extends ChessPiece {
 
+	private int row;
+	private int col;
+	private boolean hasMoved = false;
+	// Row is Y, col is X
 	public Pawn(Player player) {
 		super(player);
-		// TODO Auto-generated constructor stub
 	}
+
+	//	public Pawn(Player player, int row, int col) {
+	//		super(player);
+	//		this.row = row;
+	//		this.col = col;
+	//	}
 
 	@Override
 	public String type() {
 		return "Pawn";
 	}
 
-	public boolean isValidMove() {
+	public boolean isValidMove(Move myMove) {
 		// must utilize method from ChessPiece, add specific functionality here
-		//Move myMove = new Move(row, col, row, col + 1); // how do you access coordinates
-		//return super.isValidMove(myMove, board); // how to do access the board
+		int pom; // Plus Or Minus
+		if (super.isValidMove(myMove, board)) {
+			if (super.player() == Player.WHITE) { // white
+				pom = 1;
+			} else {
+				pom = -1;
+			}
+			if (myMove.fromRow == myMove.toRow + pom) {
+				row = myMove.toRow;
+				hasMoved = true;
+				// TODO Make sure it only moves diagonally if there's an enemy piece there
+				if (myMove.fromColumn == myMove.toColumn + 1) {
+					col = myMove.toColumn;
+					return true;
+				} else if (myMove.fromColumn == myMove.toColumn - 1) {
+					col = myMove.toColumn;
+					return true;
+				} else if (myMove.fromColumn == myMove.toColumn) {
+					return true;
+				}
+			} else if (!hasMoved && myMove.fromRow == myMove.toRow + (2 * pom)) {
+				row = myMove.toRow;
+				hasMoved = true;
+				return true;
+			}
+		}
 		return false;
 	}
+
+	//	public void setRow(int row) {
+	//		if (row <= 7 && row >= 0) {
+	//			this.row = row;
+	//		}
+	//	}
+	//	public void setColumn(int col) {
+	//		if (col <= 7 && col >= 0) {
+	//			this.col = col;
+	//		}
+	//	}
 }
