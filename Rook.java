262a7ba1b830140177632551f2a027ef8f2696db
adrianharrell123 <36007246+adrianharrell123@users.noreply.github.com>
2018-03-16 14:04:37 -0400

    Add files via upload

---
+++ Rook.java
@@ -0,0 +1,39 @@
+package Pieces;
+
+import chess.ChessPiece;
+import chess.IChessPiece;
+import chess.Move;
+import chess.Player;
+
+public class Rook extends ChessPiece {
+
+	public Rook(Player player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String type() {
+		return "Rook";
+	}
+
+	public boolean isValidMove(Move myMove, IChessPiece[][] board) {
+		// must utilize method from ChessPiece, add specific functionality here
+		// int startrow
+		// int startcol
+		// if super isvalid
+		// if tocol fromcol are equal
+		// check if anyone in the way
+			if(super.isValidMove(myMove, board) && 
+			myMove.fromRow == myMove.toRow || myMove.fromColumn == myMove.toColumn) {
+					if (moveIsOnlyOverEmptySquares(myMove, board)) {
+					return true;
+					}
+					else if (myMove.toColumn == myMove.fromColumn 
+		                    && board[myMove.toRow][myMove.toColumn].player() == super.player().next() ) {
+						
+					}
+			}
+			return false;
+	}
+}
