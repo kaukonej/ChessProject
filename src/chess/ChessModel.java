ee4b160007583a2c64a10a8cd6dceb84d1e50d45
kaukonej <36007247+kaukonej@users.noreply.github.com>
2018-03-15 20:06:47 -0400

    Filled out most methods in the class, need inCheck

--- src/chess/ChessModel.java
+++ src/chess/ChessModel.java
@@ -10,6 +10,12 @@ import Pieces.Rook;
 public class ChessModel implements IChessModel {	 
 	private IChessPiece[][] board;
 	private Player player;
+	private GameState gameState;
+	private boolean isComplete;
+	private IChessPiece[][] undoBoard1;
+	private IChessPiece[][] undoBoard2;
+	private IChessPiece[][] undoBoard3;
+	private boolean canUndo;
 	// to row
 	// to col
 	// from row
@@ -21,46 +27,50 @@ public class ChessModel implements IChessModel {
 	public ChessModel() {
 		// board is board[x][y] because that makes sense
 		// complete this
+		isComplete = false;
+		canUndo = false;
 		// Special pieces for black
-//		board[0][0] = new Rook(Player.BLACK);
-//		board[1][0] = new Knight(Player.BLACK);
-//		board[2][0] = new Bishop(Player.BLACK);
-//		board[3][0] = new King(Player.BLACK);
-//		board[4][0] = new Queen(Player.BLACK);
-//		board[5][0] = new Bishop(Player.BLACK);
-//		board[6][0] = new Knight(Player.BLACK);
-//		board[7][0] = new Rook(Player.BLACK);
-//		// Special pieces for white
-//		board[0][7] = new Rook(Player.WHITE);
-//		board[1][7] = new Knight(Player.WHITE);
-//		board[2][7] = new Bishop(Player.WHITE);
-//		board[3][7] = new King(Player.WHITE);
-//		board[4][7] = new Queen(Player.WHITE);
-//		board[5][7] = new Bishop(Player.WHITE);
-//		board[6][7] = new Knight(Player.WHITE);
-//		board[7][7] = new Rook(Player.WHITE);
-//		// Black pawns
-//		board[0][1] = new Pawn(Player.BLACK);
-//		board[1][1] = new Pawn(Player.BLACK);
-//		board[2][1] = new Pawn(Player.BLACK);
-//		board[3][1] = new Pawn(Player.BLACK);
-//		board[4][1] = new Pawn(Player.BLACK);
-//		board[5][1] = new Pawn(Player.BLACK);
-//		board[6][1] = new Pawn(Player.BLACK);
-//		board[7][1] = new Pawn(Player.BLACK);
-//		// White pawns
-//		board[0][6] = new Pawn(Player.WHITE);
-//		board[1][6] = new Pawn(Player.WHITE);
-//		board[2][6] = new Pawn(Player.WHITE);
-//		board[3][6] = new Pawn(Player.WHITE);
-//		board[4][6] = new Pawn(Player.WHITE);
-//		board[5][6] = new Pawn(Player.WHITE);
-//		board[6][6] = new Pawn(Player.WHITE);
-//		board[7][6] = new Pawn(Player.WHITE);
+		board[0][0] = new Rook(Player.BLACK);
+		board[0][1] = new Knight(Player.BLACK);
+		board[0][2] = new Bishop(Player.BLACK);
+		board[0][3] = new King(Player.BLACK);
+		board[0][4] = new Queen(Player.BLACK);
+		board[0][5] = new Bishop(Player.BLACK);
+		board[0][6] = new Knight(Player.BLACK);
+		board[0][7] = new Rook(Player.BLACK);
+		// Special pieces for white
+		board[7][0] = new Rook(Player.WHITE);
+		board[7][1] = new Knight(Player.WHITE);
+		board[7][2] = new Bishop(Player.WHITE);
+		board[7][3] = new King(Player.WHITE);
+		board[7][4] = new Queen(Player.WHITE);
+		board[7][5] = new Bishop(Player.WHITE);
+		board[7][6] = new Knight(Player.WHITE);
+		board[7][7] = new Rook(Player.WHITE);
+		// Black pawns
+		board[1][0] = new Pawn(Player.BLACK);
+		board[1][1] = new Pawn(Player.BLACK);
+		board[1][2] = new Pawn(Player.BLACK);
+		board[1][3] = new Pawn(Player.BLACK);
+		board[1][4] = new Pawn(Player.BLACK);
+		board[1][5] = new Pawn(Player.BLACK);
+		board[1][6] = new Pawn(Player.BLACK);
+		board[1][7] = new Pawn(Player.BLACK);
+		// White pawns
+		board[6][0] = new Pawn(Player.WHITE);
+		board[6][1] = new Pawn(Player.WHITE);
+		board[6][2] = new Pawn(Player.WHITE);
+		board[6][3] = new Pawn(Player.WHITE);
+		board[6][4] = new Pawn(Player.WHITE);
+		board[6][5] = new Pawn(Player.WHITE);
+		board[6][6] = new Pawn(Player.WHITE);
+		board[6][7] = new Pawn(Player.WHITE);
 	}
-	public boolean gameOver( ) {
-		return false;
+	
+	public enum GameState {
+	    IN_CHECK, NOT_IN_CHECK;
 	}
+	
 	public boolean isValidMove(Move move) {
 		// complete this
 		// if piece at "from" does not exist
@@ -70,15 +80,22 @@ public class ChessModel implements IChessModel {
 		// return true if it can, but otherwise
 		// piece = board[][];
 		// piece.isValidMove(new Move(piece.col, piece.row, piece.))
-		
-		
+
+		if (board[move.fromRow][move.fromColumn].player() == player) {
+			if (board[move.fromRow][move.fromColumn].isValidMove(move, board)) {
+				if (gameState == GameState.NOT_IN_CHECK) {
+					move(move);
+				}
+			}
+		}
+
 		// overloaded, not overridden
 		// called from the GUI
 		// if piece exists at from coords
 		// if board[fromrow][fromcol].isValidMove(move, board) == true (if move works for piece at from
 		// IChessPiece from piece = board[from][from]
 		// IChessPiece to piece = board[to][to]
-		
+
 		// boolean valid = false;
 		// YOU CANT MOVE INTO CHECK
 		// if not in check,
@@ -96,29 +113,38 @@ public class ChessModel implements IChessModel {
 	public void move(Move move) {
 		// complete this
 		// if the move is a valid move,
-			// if any pieces captured, store them somehow for later
+		// if any pieces captured, store them somehow for later
 		// set piece at "from" to "to"
-		
-		// save from row
-		// save from col
-		// save from piece
-		// save to row
-		// save to col
-		// save to piece
-		
-		// bpard[to] = board[from]
-		// board[from] = null
+
+		if (isValidMove(move)) {
+			undoBoard3 = undoBoard2;
+			undoBoard2 = undoBoard1;
+			undoBoard1 = board;
+			// maybe store captured piece if captured?
+			board[move.toRow][move.toColumn] = board[move.fromRow][move.fromColumn];
+			board[move.fromRow][move.fromColumn] = null;
+		}
+	}
+
+	public void undo() {
+		if (undoBoard1 != null) {
+			board = undoBoard1;
+			undoBoard1 = undoBoard2;
+			undoBoard2 = undoBoard3;
+			undoBoard3 = null;
+		} if (undoBoard1 == null) {
+			canUndo = false;
+		}
 	}
 	
-	// methods to do
-	// numRows()
-	//numCols
-	//PieceAt
-	//SetNextPlayer
-	//SetPiece
+	//TODO SetPiece??????
 	
-	
+	private void setNextPlayer() {
+		player = player.next();
+	}
+
 	public boolean inCheck(Player p) {
+		// TODO: Add method, use GameState.IN_CHECK and GameState.NOT_IN_CHECK
 		return false;
 	}
 	public Player currentPlayer() {
@@ -127,11 +153,11 @@ public class ChessModel implements IChessModel {
 	}
 	public int numRows() {
 		// complete this
-		return 0;
+		return 8;
 	}
 	public int numColumns() {
 		// complete this
-		return 0;
+		return 8;
 	}
 	public IChessPiece pieceAt( int row, int col ) { // parameter was originally "Square s"
 		return board[row][col];
@@ -139,7 +165,7 @@ public class ChessModel implements IChessModel {
 	@Override
 	public boolean isComplete() {
 		// TODO Auto-generated method stub
-		return false;
+		return isComplete;
 	}
 
 	// add other public or helper methods as needed
